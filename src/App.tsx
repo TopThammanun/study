4d4b20eafeb329bb9e4282c2fba4c67c14918743
top <toptoptap21@gmail.com>
2024-07-07 15:53:57 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import myImage from "./assets/IMG_7707.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Natipat</h1>
+      <img src={myImage} width={500} height={700} />
+    </div>
+  );
+}
+
+export default App;
